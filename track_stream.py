553ea31505e703e3c3af508ae17ac4bdf405d5da
wgh <3030649311@qq.com>
2024-04-18 23:14:20 +0800

    updated track_stream.py

--- track_stream.py
+++ track_stream.py
@@ -1,18 +1,23 @@
 # yolo rtsp流追踪
+import cv2
+import time
 from ultralytics import YOLO
 
 # Load the model and run the tracker with a custom configuration file
-model = YOLO(r"E:\Projects\weight\yolo\v8\detect\export\openvino\yolov8s_openvino_model_384x640")  # 35ms gpu
+model = YOLO(r"E:\Projects\weights\yolo\v8\detect\coco\yolov8s.pt")  # 35ms gpu
 
 results = model.track(
-    source="rtsp://admin:123456@192.168.31.211:554/stream1",
+    source='rtsp://192.168.31.181:8554/stream0',
+    classes=[0, 2],  # 0: person, 2: car
     tracker="bytetrack.yaml",  # 20fps
     imgsz=(384, 640),
     stream=True,
+    half=True,
     verbose=False
     )
 
 for result in results:
+    annotated_frame = result.plot()
     det = result.boxes.data.cpu().numpy()
     for *xyxy, id, conf, cls in reversed(det):
         frame = result.orig_img
